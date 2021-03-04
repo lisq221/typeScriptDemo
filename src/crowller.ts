98676087cd9d3050430801ae4edf3587209037c8
lisq221 <lisq221@chinaunicom.cn>
2021-03-04 10:29:24 +0800

    爬虫

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,5 +1,24 @@
+//node方法，判断文件是否存在
+import fs from "fs";
+import path from "path";
 //ts->.d.ts 翻译文件->js
 import superagent from "superagent";
+import cheerio from "cheerio";
+
+//创建一个接口
+interface Course {
+  title: string;
+  count: number;
+}
+
+interface courseResult {
+  time: number;
+  data: Course[];
+}
+
+interface Content {
+  [propName: number]: Course[];
+}
 
 //创建爬虫类
 class Crowller {
@@ -7,14 +26,53 @@ class Crowller {
 
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
 
-  private rawHtml = "";
+  constructor() {
+    this.initSpiderProcess();
+  }
 
+  //主业务逻辑代码
+  async initSpiderProcess() {
+    const filePath = path.resolve(__dirname, "../data/course.json");
+    const html = await this.getRawHtml();
+    const courseInfo = this.getCurseInfo(html);
+    const fileContent = this.generateJSONContent(courseInfo);
+    fs.writeFileSync(filePath, JSON.stringify(fileContent));
+  }
+
+  //获取html
   async getRawHtml() {
     const res = await superagent.get(this.url);
-    this.rawHtml = res.text;
+    return res.text;
   }
-  constructor() {
-    this.getRawHtml();
+
+  //提取html中需要的数据
+  getCurseInfo(html: string) {
+    const $ = cheerio.load(html);
+    const courseItems = $(".course-item");
+    let courseInfos: Course[] = [];
+    courseItems.map((index, element) => {
+      const descs = $(element).find(".course-desc");
+      const title = descs.eq(0).text();
+      const count = parseInt(descs.eq(1).text().split("：")[1], 10);
+      courseInfos.push({ title, count });
+    });
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos
+    };
+    return result;
+  }
+
+  //将数据生成JSON文件
+  generateJSONContent(courseInfo: courseResult) {
+    const filePath = path.resolve(__dirname, "../data/course.json");
+    let fileContent: Content = {};
+    //先读
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    }
+    fileContent[courseInfo.time] = courseInfo.data;
+    return fileContent;
   }
 }
 
